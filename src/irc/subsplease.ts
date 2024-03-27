d92bdc41b790c3a45d24c1a454a4b6ec446f14da
Flupster <esptidus@gmail.com>
2024-03-27 11:23:48 +0000

    only allow trusted subsplease

--- src/irc/subsplease.ts
+++ src/irc/subsplease.ts
@@ -1,9 +1,16 @@
+import { logger } from "../log";
 import socket from "../socket/client";
 import { bytesToHumanReadable } from "../utils";
 import bot from "./irc";
 
+const log = logger("subsplease");
+
 socket.on("release", (release) => {
   if (!release.title.includes("[SubsPlease]")) return;
+  if (!release.trusted) {
+    log.warn("untrusted subsplease release");
+    return;
+  }
 
   const size = bytesToHumanReadable(release.size).replace("i", "");
   const message = `[Release] ${release.title} (${size}) - https://nyaa.si/view/${release.id} - https://nyaa.si/view/${release.id}/torrent`;
